b7759ee41e9b77f2b3710dc71735420e996bc760
JSantt4na <jhonnsantt4na@gmail.com>
2024-07-01 09:00:59 -0300

    Aula01 - Funcionamentos Basico

--- Typescript/CFB Cursos/Aula01.js
+++ Typescript/CFB Cursos/Aula01.js
@@ -1,17 +1,11 @@
 // Ts Precisa compilar para funcionar na web Js
 // Porem Funciona com o node.js
-
 // Compilacao = tsc nomedoarquivo 
 // Apos comlilar e abri os dois arquivos juntos retornara
 // um erro que tem ja achou a variavel no mesmo lugar 
-
 // Programaçao Fortemento tipada
-
-const nome = "Jhonn";
-const curso = "CFB Cursos";
-const curso1 = "Typescript" // Qualquer Alteracao tera que compilar novamente
-
-
+var nome = "Jhonn";
+var curso = "CFB Cursos";
+var curso1 = "Typescript"; // Qualquer Alteracao tera que compilar novamente
 console.log(nome);
 console.log(curso);
- 
